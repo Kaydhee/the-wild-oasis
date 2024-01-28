2638170529af0df39fb80fa65c4fcef6a6515b12
Daniel David <dannieimeh@gmail.com>
2024-01-28 21:31:04 +0100

    working on uploading images

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://jjaxpuwrzfznpkaktevu.supabase.co';
+export const supabaseUrl = 'https://jjaxpuwrzfznpkaktevu.supabase.co';
 const supabaseKey =
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqYXhwdXdyemZ6bnBrYWt0ZXZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYxOTY2OTUsImV4cCI6MjAyMTc3MjY5NX0.r7C24ahxvs0u3VVpxu8GaNbNvdxz-aToBzrIpA-Qmzw';
 const supabase = createClient(supabaseUrl, supabaseKey);
